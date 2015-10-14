b65de0fdc28dd87692bc311ba7b802e85d8916b1
Alex <renata@Renata.wework.com>
2015-10-14 10:56:19 -0400

    exceptions

---
+++ src/main/java/Exceptions/InvalidTargetDirException.java
@@ -0,0 +1,22 @@
+package Exceptions;
+
+public class InvalidTargetDirException extends Exception{
+
+	private static final long serialVersionUID = -2099065287465810719L;
+	
+	public InvalidTargetDirException() {
+	}
+	
+	public InvalidTargetDirException(String message){
+		super(message);
+	}
+	
+	public InvalidTargetDirException(Throwable cause){
+		super(cause);
+	}
+	
+	public InvalidTargetDirException(String message, Throwable cause){
+		super(message,cause);
+	}
+
+}
